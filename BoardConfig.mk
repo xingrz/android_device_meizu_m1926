21f1a568316dd1510ffbdf2cdfc2fd80662ad0c9
XiNGRZ <chenxingyu92@gmail.com>
2020-01-03 17:55:01 +0800

    m1926: Asserts for stock ro.product.device
    Change-Id: I66fd175d7a9430f797019ed4dceff8a64d7716a0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@
 
 DEVICE_PATH := device/meizu/m1926
 
+# Assertion
+TARGET_OTA_ASSERT_DEVICE := m1926,meizu16Xs
+
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
